1a468274b0cc9f59d2869886b2661127bf8b6cdb
Asha <itssharmilaasha1@gmail.com>
2021-10-01 20:56:34 +0800

    main

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -7,11 +7,11 @@ elNamaPengguna.addEventListener("keyup",function(){
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
 })
 
-functioncheckPassword(){
+function checkPassword(){
     let elKataLaluan = document.getElementById("kataLaluan")
     let pas_val = elKataLaluan.value
 
-    if(pass_val.length > 5){
+    if(pas_val.length > 5){
         alert("Password check...OK!")
     }else {
         alert("Password too short!")
