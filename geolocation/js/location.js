a6d6eb5bde08847b33afb320b6d2ba52215a052f
Asha <itssharmilaasha1@gmail.com>
2021-10-04 20:15:37 +0800

    added geolocation

---
+++ geolocation/js/location.js
@@ -0,0 +1,6 @@
+let elLocate = document.getElementById("btnLocate")
+elLocate.addEventListener("click",function(position){
+    let elLat = document.getElementById("lat")
+    let elLong = dociment.getElementById("long")
+    elLong.innerHTML = "Latitude: " + 
+}
