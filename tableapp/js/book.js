9c57e97b6d32ea89dec63170acdd9da9b6a4a0e4
Asha <itssharmilaasha1@gmail.com>
2021-10-11 20:54:32 +0800

    updated js booking

--- tableapp/js/book.js
+++ tableapp/js/book.js
@@ -19,19 +19,25 @@ function BookNow(userName, userEmail, userPax, userRemarks) {
     let url = 'https://api.sheety.co/1ad25830927a93168640bade1ef761fb/bookingApp/bookings';
     let body = {
         booking: {
-            name: userName
-            email: userEmail
-            pax: userPax
+            name: userName,
+            email: userEmail,
+            pax: userPax,
             remarks: userRemarks
       }
     }
     fetch(url, {
         method: 'POST',
-        body: JSON.stringify(body)
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/jason"
+        }
     })
         .then((response) => response.json())
         .then(json => {
             // Do something with object
             console.log(json.booking);
+            if(json.booking.length == 1) {
+                alert(json.booking.name + " added in the list!")
+            }
         });
 }
