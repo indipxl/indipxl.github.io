38164c81e44f286240a17a9e38cb3942617ef8c2
Asha <itssharmilaasha1@gmail.com>
2021-11-08 10:27:55 +0800

    edited

--- minMaxAvg/js/index.js
+++ minMaxAvg/js/index.js
@@ -1,1 +1,17 @@
-const arr = [33, 4, 6, 15, 42];
+var arr = [7, 5, 12, 1, -3, 8];
+
+var min = arr[0];
+var max = arr[0];
+var average = 0;
+
+for(var i = 0; i < arr.length; i++) {
+  if(arr[i] < min) min = arr[i];
+  if(arr[i] > max) max = arr[i];
+  average += arr[i];
+}
+
+average /= arr.length;
+
+console.log("Min is : " + min);
+console.log("Max is : " + max);
+console.log("Average is : " + average);
